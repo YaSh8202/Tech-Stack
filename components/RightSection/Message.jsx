bfb94a354affaad920ab2d056c1caed4158523fb
Yash <bajajy82@gmail.com>
2022-10-16 16:36:13 +0530

    added markdown previewer in  messages

--- components/RightSection/Message.jsx
+++ components/RightSection/Message.jsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 import React, { useContext, useEffect, useState } from "react";
 import { firestore } from "../../lib/firebase";
 import { UserContext } from "../../lib/userContext";
+import MdEditor from "react-markdown-editor-lite";
+// Import styles
+import "react-markdown-editor-lite/lib/index.css";
+// Two different markdown parser
+import MarkdownIt from "markdown-it";
+const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 const Message = ({ message }) => {
   const [sender, setSender] = useState(null);
@@ -22,7 +28,14 @@ const Message = ({ message }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (message.isMarkdown === true) return;
+  function renderHTML(text) {
+    // Using markdown-it
+    return mdParser.render(text);
+    // Using react-markdown
+    // return React.createElement(ReactMarkdown, {
+    //   source: text,
+    // });
+  }
 
   return (
     <div
@@ -31,8 +44,15 @@ const Message = ({ message }) => {
         isSender ? "ml-auto bg-[#D7F8F4] " : "mr-auto bg-white "
       } my-2 px-3 py-2 rounded-md `}
     >
-      <div className="flex flex-row gap-2 items-center">
-        {/* <Image
+      {message.isMarkdown === true ? (
+        <MdEditor
+          value={message.text}
+          view={{ menu: false, md: false, html: true }}
+          renderHTML={renderHTML}
+        />
+      ) : (
+        <div className="flex flex-row gap-2 items-center">
+          {/* <Image
           src={sender?.photoURL}
           alt="sender"
           className="w-10 h-10 rounded-full"
@@ -40,23 +60,24 @@ const Message = ({ message }) => {
           width={40}
           height={40}
         /> */}
-        <div className="flex flex-col">
-          {!isSender && (
-            <p className="text-sm font-semibold">{sender?.username}</p>
+          <div className="flex flex-col">
+            {!isSender && (
+              <p className="text-sm font-semibold">{sender?.username}</p>
+            )}
+            <p className="text-sm whitespace-pre-wrap ">{message.text}</p>
+            {/* {NewlineText({ text: message.text })} */}
+          </div>
+          {message.img && (
+            <div className="relative w-80 h-64 ">
+              <Image
+                layout="fill"
+                src={message.img}
+                alt="message"
+                className="rounded-md"
+                objectFit="cover"
+              />
+            </div>
           )}
-          <p className="text-sm whitespace-pre-wrap ">{message.text}</p>
-          {/* {NewlineText({ text: message.text })} */}
-        </div>
-      </div>
-      {message.img && (
-        <div className="relative w-80 h-64 ">
-          <Image
-            layout="fill"
-            src={message.img}
-            alt="message"
-            className="rounded-md"
-            objectFit="cover"
-          />
         </div>
       )}
     </div>
