562696df9f2ac793b4a087456f753751d9f31907
Yash <bajajy82@gmail.com>
2022-10-21 14:28:52 +0530

    removed SSR for groups

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
-import dynamic from "next/dynamic";
 import Head from "next/head";
 import LoginModal from "../components/Modals/LoginModal";
 import Sidebar from "../components/Sidebar";
 import RightSection from "../components/RightSection/RightSection";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import { firestore, postToJSON } from "../lib/firebase";
-import { GroupContext } from "../lib/groupContext";
-import { useContext, useEffect } from "react";
+
 
 export default function Home({ groups }) {
-  const { setGroups } = useContext(GroupContext);
-
-  useEffect(() => {
-    setGroups(groups);
-  }, []);
-
+  
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-row min-w-full  ">
       <Head>
@@ -27,23 +18,23 @@ export default function Home({ groups }) {
   );
 }
 
-export async function getStaticProps() {
-  const groupsQuery = query(
-    collection(firestore, "Groups"),
-    orderBy("updatedAt", "desc")
-  );
+// export async function getStaticProps() {
+//   const groupsQuery = query(
+//     collection(firestore, "Groups"),
+//     orderBy("updatedAt", "desc")
+//   );
 
-  const querySnapshot = await getDocs(groupsQuery);
-  const groups = [];
-  querySnapshot?.forEach((doc) => {
-    groups.push(postToJSON(doc));
-  });
+//   const querySnapshot = await getDocs(groupsQuery);
+//   const groups = [];
+//   querySnapshot?.forEach((doc) => {
+//     groups.push(postToJSON(doc));
+//   });
 
-  // console.log(groups);
-  return {
-    props: {
-      groups,
-    },
-    // revalidate: 1000,
-  };
-}
+//   // console.log(groups);
+//   return {
+//     props: {
+//       groups,
+//     },
+//     // revalidate: 1000,
+//   };
+// }
